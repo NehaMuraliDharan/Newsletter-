41c7c6e0bcff88cebd87bb086e22bd915f8f6584
Neha Muralidharan <62611122+NehaMuraliDharan@users.noreply.github.com>
2020-06-10 15:12:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express= require("express");
+const bodyParser= require("body-parser");
+const request= require("request");
+const https= require("https");
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+app.listen(3000,function(){
+  console.log("Voila! its running boi");
+});
+app.post("/",function(req,res){
+  const FirstName=req.body.Fname;
+  const LastName=req.body.Lname;
+  const email=req.body.email;
+  const data={
+    members:[
+      {
+        email_address: email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:FirstName,
+          LNAME:LastName
+        }
+      }
+    ]
+  };
+  const jsonData=JSON.stringify(data);
+  const url= 'https://us4.api.mailchimp.com/3.0/lists/7a9cabe2f9';
+  const options={
+    method:"POST",
+    auth: "NehaMuralidharan:6e301e36a577bbc1544cf9f2157bf3de-us4s"
+  }
+  const request= https.request(url,options,function(response){
+    if(response.statusCode===200){
+      res.sendFile(__dirname+"/success.html");
+    }
+    else 
+    {
+      res.sendFile(__dirname+"/failure.html");
+    }
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
